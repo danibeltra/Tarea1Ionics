1b1d89712ca35ff1f9f815b9cf6b8ffdb1b8e55a
DaniBeltrá <dbg4@alu.ua.es>
2019-01-12 23:02:38 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import {
+  Pagina2Page,
+  Tab1Page,
+  Tab2Page,
+  Tab3Page,
+  TabsPage
+} from "../pages/index.paginas";
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Pagina2Page,
+    Tab1Page,
+    Tab2Page,
+    Tab3Page,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      backButtonText: 'Atras'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Pagina2Page,
+    Tab1Page,
+    Tab2Page,
+    Tab3Page,
+    TabsPage
   ],
   providers: [
     StatusBar,
